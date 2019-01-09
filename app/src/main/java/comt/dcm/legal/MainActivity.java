713d8da197d5282846e3aab0f3c2f34091d228f1
Vikash Kumar <vikash.k@dcmtech.com>
2019-01-09 10:40:56 +0530

    First commit

---
+++ app/src/main/java/comt/dcm/legal/MainActivity.java
@@ -0,0 +1,27 @@
+package comt.dcm.legal;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.webkit.WebSettings;
+import android.webkit.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("http://192.168.1.2:8080/LegalApp/login");  //enter url or domain here with http/https
+
+
+    }
+
+}
